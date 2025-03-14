9bf8d657343b71c78e593577d99b78232b3df4a0
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-03-14 18:08:44 +0100

    corses

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true, // Important if you use cookies, authorization headers, etc.
+  });
+
   await app.listen(process.env.PORT || 3025);
 }
 bootstrap();
