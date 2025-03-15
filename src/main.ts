d041ee9543812becd6102e30cd8c8a702fb716c1
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-03-15 12:29:39 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://fe-budget-tracker.onrender.com/',
+    ],
     credentials: true, // Important if you use cookies, authorization headers, etc.
   });
 
