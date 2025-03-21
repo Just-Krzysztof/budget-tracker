7d2ce5996d69d5365b86cada59bc4024aa8a5347
Krzysztof <krzysztofjustgdynia@gmail.com>
2025-03-21 18:35:47 +0100

    removed slash

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'http://localhost:5173/',
       'https://fe-budget-tracker-production.up.railway.app',
-      'https://fe-budget-tracker-production.up.railway.app/',
       'https://track-expenses.up.railway.app',
-      'https://track-expenses.up.railway.app/',
     ],
     credentials: true,
   });
